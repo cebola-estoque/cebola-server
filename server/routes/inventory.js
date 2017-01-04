070f1f2b5e27ca4ec3b6d18f91221ea864316af4
Simon <simonxd@gmail.com>
2017-01-04 17:20:36 -0200

    inventory summary

--- server/routes/inventory.js
+++ server/routes/inventory.js
@@ -1,16 +1,18 @@
 module.exports = function (app, options) {
 
-  // app.get('/inventory/summary',
-  //   app.middleware.authenticate(),
-  //   function (req, res, next) {
-  //     app.controllers.inventory
-  //       .computeSummary()
-  //       .then((summary) => {
-  //         res.json(summary);
-  //       })
-  //       .catch(next);
-  //   }
-  // );
+  // TODO: auth
+  app.get('/inventory/available-products',
+    // app.middleware.authenticate(),
+    function (req, res, next) {
+      app.controllers.inventory
+        .summary(null, null, null, { keepRecords: true })
+        .then((summary) => {
+          console.log('summary', summary);
+          res.json(summary);
+        })
+        .catch(next);
+    }
+  );
 
   // app.get('/inventory/search',
   //   app.middleware.authenticate(),
