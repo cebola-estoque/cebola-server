f524d6ce192adeda02a10e6812e4601b47153687
Simon <simonxd@gmail.com>
2017-10-02 18:56:48 -0300

    productModel categories

--- server/models/schema-mixins/product-model.js
+++ server/models/schema-mixins/product-model.js
@@ -7,7 +7,39 @@ module.exports = function (app, options) {
         url: String,
         size: Number,
         mimetype: String,
+      },
+
+      categories: [{
+        name: String,
+      }],
+    });
+
+    /**
+     * Normalize data before validation is run
+     */
+    productModelSchema.pre('validate', function (next) {
+      /**
+       * Ensure the categories is in UPPERCASE
+       */
+      if (this.categories && Array.isArray(this.categories)) {
+        this.categories = this.categories.map((cat) => {
+          if (typeof cat.name === 'string') {
+            return {
+              name: cat.name.toUpperCase(),
+            };
+          } else {
+            return undefined;
+          }
+        })
+        .filter((cat) => {
+          return cat ? true : false;
+        });
+
+      } else {
+        this.categories = [];
       }
+
+      next();
     });
 
     productModelSchema.index({
